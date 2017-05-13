4fc9ad4a8a520b8cf5b6278cef438a7cf6935e12
github username <github email address>
2017-05-13 00:04:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //Code React element here
+
+const attributes =
+  React.createElement('ul', { className: 'me__interests'},
+      [
+        React.createElement('li', {}, 'JavaScript'),
+        React.createElement('li', {}, 'React'),
+        React.createElement('li', {}, 'Movies'),
+        React.createElement('li', {}, 'Ice cream')
+      ]);
+
+const hOne = React.createElement('h1', {}, 'An Awesome Person');
+const p = React.createElement('p', {}, 'Who is learning React');
+
+const meInReact = React.createElement('div', { className: 'me' }, [hOne, p, attributes]);
